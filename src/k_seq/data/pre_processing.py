75051c37029e2e1e2a8c15a7d2d04aac4ac09223
Yuning Shen <ynshen23@gmail.com>
2019-04-15 18:39:30 -0700

    fix x_values problem

--- src/k_seq/data/pre_processing.py
+++ src/k_seq/data/pre_processing.py
@@ -98,7 +98,7 @@ class SequencingSample:
         :return:
         """
 
-        self.quant_factor = spike_in_amount * self.total_counts / np.sum(self.spike_in_counts[:max_dist + 1])
+        self.quant_factor = spike_in_amount * self.total_counts / np.sum(self.spike_in['spike_in_counts'][:max_dist + 1])
         self.spike_in['quant_factor_max_dist'] = max_dist
         self.spike_in['spike_in_amount'] = spike_in_amount
 
@@ -210,7 +210,7 @@ def get_file_list(file_root, pattern=None):
     return sample_list
 
 
-def load_count_files(file_root, pattern=None, name_pattern=None, sort_fn=None, black_list=[], silent=True):
+def load_count_files(file_root, x_values, sample_list=None, pattern=None, name_pattern=None, sort_fn=None, black_list=[], silent=True):
     """
     load all count files under file_root if comply with pattern. A list of SequencingSample will return, each includes
         self.file_dirc: full directory to the count file
@@ -220,6 +220,8 @@ def load_count_files(file_root, pattern=None, name_pattern=None, sort_fn=None, b
         self.sequences: a dictionary of {seq: count} reported in count file
         self.sample_type: type of sample, either be 'input' or 'reacted'
     :param file_root: root directory
+    :param x_values: string or a list of floats. if string, will use it as domain name to extract x_value for each sample
+                     if a list of floats, it should have same length and order as the sample list
     :param pattern: optional, file name pattern to identify count files
     :param name_pattern: optional. pattern to extract metadata. pattern rules: [...] to include the region of
                          sample_name, {domain_name[, digit]} to indicate region of domain to extract as metadata, including
@@ -236,19 +238,25 @@ def load_count_files(file_root, pattern=None, name_pattern=None, sort_fn=None, b
     :param slient: boolean, false: print calculation progress to the screen
     :return: sample_set: a list of SequencingSample class
     """
-    sample_list = get_file_list(file_root=file_root, pattern=pattern)
+    if sample_list is None:
+        sample_list = get_file_list(file_root=file_root, pattern=pattern)
+        print("NOTICE: no sample_list is given, samples will extract automaticall from file_root.")
+        if type(x_values) != str:
+            raise Exception("No sample_list is given, please indicate domain name instead of list of real values to extract x_values")
     sample_set = []
-    for sample_name in sample_list:
+    if type(x_values) == str:
+        x_values = [x_values for _ in sample_list]
+    for sample_ix,sample_name in enumerate(sample_list):
         if sample_name not in black_list:
             sample = SequencingSample(file_root=file_root,
                                       sample_name=sample_name,
+                                      x_value=x_values[sample_ix],
                                       name_pattern=name_pattern,
                                       silent=silent)
             sample_set.append(sample)
     if sort_fn:
         sample_set.sort(key=sort_fn)
     return sample_set
-
 
 def get_quant_factors(sample_set, spike_in='AAAAACAAAAACAAAAACAAA', max_dist=2, max_dist_to_survey=10,
                       spike_in_amounts=None, manual_quant_factor=None, silent=True):
