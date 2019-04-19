ca17a67398e253657bb9e12da807682dcc8b6e64
Yuning Shen <ynshen23@gmail.com>
2019-04-18 22:26:57 -0700

    add bootstrap option to set x to be not fixed

--- src/k_seq/fitting/fitting.py
+++ src/k_seq/fitting/fitting.py
@@ -1,77 +1,85 @@
-def get_fitting_data(seq, sequence_set=None, black_list=None, na_data_exclude=True):
-    """
-    get the x and y corresponding to seq for fitting, from pd.DataFrame .reacted_frac_table
-    :param seq: the r
-    :return:
-    """
-    if type(seq) is str:
-        seq = sequence_set.reacted_frac_table['seq']
-    if black_list:
-        black_list += [name if sequence_set.sample_info[name]['sample_type'] == 'input' for name in list(seq.index)]
+"""
+Methods needed for fitting
+"""
+
+from scipy.optimize import curve_fit
+import numpy as np
 
 
+def func_default(x, A, k):
+    return A * (1 - np.exp(- 0.479 * 90 * k * x))  # BYO degradation adjustment and 90 minutes
 
 
+def fitting_single(x_data, y_data, func=func_default, weights=None, bounds=None,
+                   ci_est=True, bs_depth=500, bs_return_all=True, bs_fix_x=False,
+                   missing_data_as_zero=False, y_max=None):
+    from inspect import signature
 
-
-def fitting(seq, xdata, maxFold=None, fitMtd='trf', ciEst=True, func=None, bsDepth=500):
-    """
-    :param seq: the sequence to fit
-    :param xdata: the x-value, corresponding to the list of ALL samples
-    :param maxFold: if the maximum reacted fraction can exceed the indicated value, default None
-    :param fitMtd: fitting method to use
-    :param ciEst: If the confidence interval will be estimated by bootstrapping
-    :param func: func to fit, if None will fit to default exponential function
-    :return: seq with fitting results
-    """
-    from scipy.optimize import curve_fit
-    import numpy as np
-
-    def func_default(x, A, k):
-        return A * (1 - np.exp(- 0.479 * 90 * k * x))  # BTO degradation adjustment and 90 minutes
-
-    if func is None:
-        func = func_default
-    ydata = np.array(seq['reactedFrac'][1:])
-    if not(maxFold is None):
-        ydata = np.array([min(yi, maxFold) for yi in ydata])
-    valid = ~np.isnan(ydata)
+    # get number of args in func
+    sig = signature(func)
+    param_num = len(sig.parameters) - 1
+    # initialize data to fit
+    if y_max is not None:
+        y_data = np.array([min(yi, y_max) for yi in y_data])
+    if missing_data_as_zero:
+        y_data[np.isnan(y_data)] = 0
+    if not weights:
+        weights = np.ones(len(y_data))
+    valid = ~np.isnan(y_data)
+    x_data = x_data[valid]
+    y_data = y_data[valid]
+    weights = weights[valid]
+    results = {
+        'x_data': x_data,
+        'y_data': y_data,
+        'fitting_weights': weights
+    }
     try:
-        initGuess = [np.random.random(), np.random.random()]
-        params, pcov = curve_fit(func, xdata=xdata[valid], ydata=ydata[valid],
-                                 method=fitMtd, bounds=([0, 0], [1., np.inf]), p0=initGuess)
-        x_ = xdata[valid]
-        y_ = ydata[valid]
-        yHat = func(x_, params[0], params[1])
-        res = y_ - yHat
-        pctRes = res / yHat
+        init_guess = [np.random.random() for _ in range(param_num)]
+        results['params'], results['pcov'] = curve_fit(func, xdata=x_data, ydata=y_data, sigma=weights,
+                                                       method='trf', bounds=bounds, p0=init_guess)
+        y_hat = func(x_data, *results['params'])
+        res = y_data - y_hat
+        results['pct_res'] = res / y_hat
         ss_res = np.sum(res ** 2)
-        ss_tot = np.sum((ydata[valid] - np.mean(ydata[valid])) ** 2)
-        seq['r2'] = (1 - ss_res / ss_tot)
+        ss_tot = np.sum((y_data - np.mean(y_data)) ** 2)
+        results['r2'] = (1 - ss_res / ss_tot)
     except RuntimeError:
-        params = [np.nan, np.nan]
-    seq['params'] = [params[0], params[1], params[0]*params[1]]
-    if ciEst:
-        if (len(xdata[valid])>1)and(~np.isnan(params[0])):
-            paramList = []
-            for i in range(bsDepth):
-                pctResResampled = np.random.choice(pctRes, replace=True, size=len(pctRes))
-                yToFit = yHat + yHat * pctResResampled
-                try:
-                    initGuess = (np.random.random(), np.random.random())
-                    params, pcov = curve_fit(func, x_, yToFit,
-                                             method=fitMtd, bounds=([0, 0], [1., np.inf]), p0=initGuess)
-                except:
-                    params = [np.nan, np.nan]
-                    print(x_, yToFit)
-                paramList.append([params[0], params[1], params[0] * params[1]])
-            seq['stdevs'] = np.nanstd(paramList, axis=0, ddof=1)
-            seq['ci95'] = np.array([np.percentile(paramList, 2.5, axis=0), np.percentile(paramList, 97.5, axis=0)]).T
-        else:
-            seq['stdevs'] = [np.nan]
-            seq['ci95'] = [np.nan]
+        results['params'] = [np.nan for _ in range(param_num)]
 
-    return seq
+    if ci_est:
+        if (len(x_data) > 1)and(~np.isnan(results['params'][0])):
+            param_list = []
+            for _ in range(bs_depth):
+                if bs_fix_x:
+                    pct_res_resampled = np.random.choice(results['pct_res'], replace=True, size=len(results['pct_res']))  # need to change to generic form
+                    y_data_bs = y_hat * (1 + pct_res_resampled)
+                    x_data_bs = x_data
+                else:
+                    indeces = np.linspace(0, len(x_data) - 1, len(x_data))
+                    bs_indeces = np.random.choice(a=indeces, size=len(x_data), replace=True)
+                    x_data_bs = np.array([x_data[int(i)] for i in bs_indeces])
+                    y_data_bs = np.array([y_data[int(i)] for i in bs_indeces])
+                try:
+                    init_guess = [np.random.random() for _ in range(param_num)]
+                    params, pcov = curve_fit(func, xdata=x_data_bs, ydata=y_data_bs,
+                                             method='trf', bounds=bounds, p0=init_guess)
+                except:
+                    params = [np.nan for _ in range(param_num)]
+                param_list.append(params)
+            if bs_return_all:
+                results['bs_params'] = param_list
+            results['mean'] = np.nanmean(param_list, axis=0)
+            results['sd'] = np.nanstd(param_list, axis=0, ddof=1)
+            results['percentiles'] = {
+                '2.5': np.percentile(param_list, 2.5, axis=0),
+                '50': np.percentile(param_list, 50, axis=0),
+                '97.5': np.percentile(param_list, 97.5, axis=0)
+            }
+        else:
+            results['sd'] = np.nan
+
+    return results
 
 
 
