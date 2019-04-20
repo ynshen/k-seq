234a28c69be73879c033d0a1204a3286436faec5
Yuning Shen <ynshen23@gmail.com>
2019-04-19 17:31:34 -0700

    add parallelled k-seq implementation

--- src/k_seq/fitting/fitting.py
+++ src/k_seq/fitting/fitting.py
@@ -4,25 +4,36 @@ Methods needed for fitting
 
 from scipy.optimize import curve_fit
 import numpy as np
+import pandas as pd
+import multiprocessing as mp
 
 
 def func_default(x, A, k):
     return A * (1 - np.exp(- 0.479 * 90 * k * x))  # BYO degradation adjustment and 90 minutes
 
 
-def fitting_single(x_data, y_data, func=func_default, weights=None, bounds=None,
-                   ci_est=True, bs_depth=500, bs_return_all=True, bs_fix_x=False,
-                   missing_data_as_zero=False, y_max=None):
+def get_args_num(func):
     from inspect import signature
-
-    # get number of args in func
     sig = signature(func)
     param_num = len(sig.parameters) - 1
-    # initialize data to fit
+    return param_num
+
+
+def fitting_single(x_data, y_data, func=func_default, weights=None, bounds=None,
+                   ci_est=True, bs_depth=1000, bs_return_verbose=True, bs_fix_x=False,
+                   missing_data_as_zero=False, y_max=None):
+
+    # get number of args in func
+    param_num = get_args_num(func)
+    # initialize data for fitting
+    x_data = np.array(x_data)
+    y_data = np.array(list(y_data)) #regularize format if not np.array
     if y_max is not None:
         y_data = np.array([min(yi, y_max) for yi in y_data])
     if missing_data_as_zero:
         y_data[np.isnan(y_data)] = 0
+    if bounds is None:
+        bounds = [[-np.inf for _ in range(param_num)], [np.inf for _ in range(param_num)]]
     if not weights:
         weights = np.ones(len(y_data))
     valid = ~np.isnan(y_data)
@@ -41,18 +52,19 @@ def fitting_single(x_data, y_data, func=func_default, weights=None, bounds=None,
         y_hat = func(x_data, *results['params'])
         res = y_data - y_hat
         results['pct_res'] = res / y_hat
-        ss_res = np.sum(res ** 2)
-        ss_tot = np.sum((y_data - np.mean(y_data)) ** 2)
-        results['r2'] = (1 - ss_res / ss_tot)
+        # r2 calculation deprecated
+        # ss_res = np.sum(res ** 2)
+        # ss_tot = np.sum((y_data - np.mean(y_data)) ** 2)
+        # results['r2'] = (1 - ss_res / ss_tot)
     except RuntimeError:
         results['params'] = [np.nan for _ in range(param_num)]
 
     if ci_est:
+        param_list = []
         if (len(x_data) > 1)and(~np.isnan(results['params'][0])):
-            param_list = []
             for _ in range(bs_depth):
                 if bs_fix_x:
-                    pct_res_resampled = np.random.choice(results['pct_res'], replace=True, size=len(results['pct_res']))  # need to change to generic form
+                    pct_res_resampled = np.random.choice(results['pct_res'], replace=True, size=len(results['pct_res']))
                     y_data_bs = y_hat * (1 + pct_res_resampled)
                     x_data_bs = x_data
                 else:
@@ -67,19 +79,59 @@ def fitting_single(x_data, y_data, func=func_default, weights=None, bounds=None,
                 except:
                     params = [np.nan for _ in range(param_num)]
                 param_list.append(params)
-            if bs_return_all:
-                results['bs_params'] = param_list
+
             results['mean'] = np.nanmean(param_list, axis=0)
             results['sd'] = np.nanstd(param_list, axis=0, ddof=1)
-            results['percentiles'] = {
-                '2.5': np.percentile(param_list, 2.5, axis=0),
-                '50': np.percentile(param_list, 50, axis=0),
-                '97.5': np.percentile(param_list, 97.5, axis=0)
-            }
+            results['p2.5'] = np.percentile(param_list, 2.5, axis=0)
+            results['p50'] = np.percentile(param_list, 50, axis=0)
+            results['p97.5'] = np.percentile(param_list, 97.5, axis=0)
         else:
             results['sd'] = np.nan
+    if bs_return_verbose:
+        return results, param_list
+    else:
+        return results, None
 
-    return results
+
+def fitting_master(seq, x_data, func=func_default, weights=None, bounds=None,
+                   ci_est=True, bs_depth=1000, bs_return_verbose=True, bs_fix_x=False,
+                   missing_data_as_zero=False, y_max=None):
+
+    single_res = fitting_single(x_data=x_data,
+                                y_data=list(seq[1]),
+                                func=func, weights=weights, bounds=bounds,
+                                ci_est=ci_est, bs_depth=bs_depth, bs_return_verbose=bs_return_verbose,
+                                bs_fix_x=bs_fix_x, missing_data_as_zero=missing_data_as_zero, y_max=y_max)
+
+    return pd.Series(single_res[0], name=seq[0]), (seq[0], single_res[1])
+
+
+def fitting_sequence_set(sequence_set, func=func_default, weights=None, bounds=None,
+                         ci_est=True, bs_depth=1000, bs_return_verbose=True, bs_fix_x=False,
+                         missing_data_as_zero=False, y_max=None,
+                         parallel_threads=None, inplace=True):
+    from functools import partial
+
+    partial_fun = partial(fitting_master,
+                          x_data=sequence_set.reacted_frac_table.col_x_values,
+                          func=func, weights=weights, bounds=bounds,
+                          ci_est=ci_est, bs_depth=bs_depth, bs_return_verbose=bs_return_verbose,
+                          bs_fix_x=bs_fix_x, missing_data_as_zero=missing_data_as_zero, y_max=y_max)
+    if parallel_threads:
+        pool = mp.Pool(processes=int(parallel_threads))
+        results = pool.map(partial_fun, sequence_set.reacted_frac_table.iterrows())
+    else:
+        results = [partial_fun(seq) for seq in sequence_set.reacted_frac_table.iterrows()]
+
+    if inplace:
+        sequence_set.fitting_results = pd.DataFrame([res[0] for res in results])
+        if bs_return_verbose:
+            sequence_set.bs_log = {res[1][0]:res[1][1] for res in results}
+    else:
+        if bs_return_verbose:
+            return pd.DataFrame([res[0] for res in results]), {res[1][0]:res[1][1] for res in results}
+        else:
+            return pd.DataFrame([res[0] for res in results])
 
 
 
