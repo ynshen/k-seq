d76d0b4c99967ad58916ef48358f4d4fe1f8dd77
Yuning Shen <ynshen23@gmail.com>
2019-08-30 11:33:47 -0700

    add byo pipeline

---
+++ examples/abe_byo_complete_pipeline.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+import argparse
+
+# Arguments
+
+
+
+
+# Read in the count file
+
+
+# Calculate the normalization factors
+
+
+# Convert to valid sequences
+
+
+
+# Filter sequences
+
+
+
+
+# Fitting and export
