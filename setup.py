68fadc2fb6108c75b06da6cf44c8a49f36dadad1
Yuning Shen <ynshen23@gmail.com>
2020-03-04 10:56:51 -0800

    update readme

--- setup.py
+++ setup.py
@@ -1,19 +1,34 @@
-#!/usr/bin/env python3
+"""Setup.py for k-seq
+See: https://github.com/ynshen/Landscape
+"""
 
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='k-seq',
-    version='0.3.0-dev',
-    description='Package for kinetic sequencing analysis, Chen lab at University of California, Santa Barbara',
+    name='k-seq',   # this is the name registered on pypi
+    version='0.4.0a',
+    description='Kinetic sequencing analysis by Chen lab at University of California, Santa Barbara',
+    long_description=long_description,
+    long_description_content_type='text/markdown', # to use markdown, make sure wheel, setuptools, twine are up to date
     url='https://github.com/ynshen/k-seq',
     author='Yuning Shen',
     author_email='ynshen23@gmail.com',
-    packages=find_packages('src'),
+    keywords='bioinformatics sequencing fitting',
     package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.5',
     install_requires=[
-        'numpy',
         'pandas',
+        'numpy',
+        'scipy',
         'matplotlib',
         'python-levenshtein',
         'scipy',
@@ -22,5 +37,11 @@ setup(
         'seq-landscape',
         'yutility'
     ],
-    zip_safe=False
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/ynshen/k-seq/issues/',
+        'Source': 'https://github.com/ynshen/k-seq',
+    },
 )
