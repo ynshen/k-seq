9b0cbd97e8a70b74d9ed023e89968f0a5c9535b5
Yuning Shen <ynshen23@gmail.com>
2020-04-05 11:39:43 -0700

    remove scripts

--- scripts/raw-reads-to-count.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bash proto.pipeline.v3.sh \
-  -i /mnt/storage/projects/k-seq/input/byo_doped/raw_reads/ \
-  -o /mnt/storage/projects/k-seq/working/byo_doped/read_join/no-mismatch/ \
-  -p CTACGAATTC \
-  -q CTGCAGTGAA \
-  -c -T 4
-
--- scripts/tests/create-test-data.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-"""
-Script to generate data for test
-
-"""
