1e41655eac39544bcd0305f23959d95583fb8862
Yuning Shen <ynshen23@gmail.com>
2020-12-10 15:41:45 -0500

    update byo_doped.py

--- scripts/generate-byo-enriched-dataset.py
+++ scripts/generate-byo-enriched-dataset.py
@@ -14,10 +14,6 @@ from k_seq.utility.file_tools import dump_pickle, check_dir
 
 def main():
     dataset = load_byo_selected(from_count_file=True, count_file_path=args.count_file_dir, norm_path=args.norm_file)
-    if not args.full_dataset:
-        del dataset.table.original
-        del dataset.table.no_failed
-        del dataset.table.nf_filtered_reacted_frac_seq_in_all
     dump_pickle(dataset, args.output)
 
 
@@ -33,9 +29,6 @@ if __name__ == '__main__':
                         help='Normalization file from total DNA quantification')
     parser.add_argument('--output', type=str, required=True,
                         help='Folder to save `byo-variant.pkl`')
-    parser.add_argument('--full_dataset', action='store_true',
-                        help='If save the full dataset (large, ~1.7G).'
-                             'By default, only data necessary to reproduce the paper is saved')
     args = parser.parse_args()
     args.output = Path(args.output).absolute()
     check_dir(args.output)
